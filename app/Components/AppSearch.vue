9e01ecb716862cd785728b77904a758990f7406c
vitkarino <kisvik2004@gmail.com>
2025-08-23 14:05:03 +0300

    initial commit

---
+++ app/Components/AppSearch.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {GamePickerUI} from '~/Controllers/GamePickerUI'
+
+const UI = new GamePickerUI();
+</script>
+
+<template>
+	<form @submit.prevent>
+		<input
+			v-model="input"
+			type="text"
+			placeholder="Enter your Steam ID, custom URL, or Steam profile URL"
+			autocomplete="off"
+			spellcheck="false"
+		/>
+		<button
+			type="submit"
+			@click="UI.searchUser(input)"
+			>
+			Search
+		</button>
+	</form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/Controllers/GamePickerUI.ts
@@ -0,0 +1,9 @@
+export class GamePickerUI {
+    constructor() {
+        console.log("GamePickerUI initialized");
+    }
+
+    public searchUser(query: string): void {
+        console.log(`Searching for user with query: ${query}`);
+    }
+}
