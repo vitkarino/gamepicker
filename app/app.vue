4d9fff74f13a9c2d7a2b3efe53b30528cf0793f2
vitkarino <kisvik2004@gmail.com>
2025-08-23 14:05:21 +0300

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+	  <AppHeader />
+	  <AppSearch />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from './Components/AppHeader.vue'
+import AppSearch from './Components/AppSearch.vue'
+</script>
