fd9c2503518a15fda9ba8f2843562c3edea3f460
vitkarino <kisvik2004@gmail.com>
2025-08-24 13:47:49 +0300

    implement user search with debounce and display user info

--- app/Components/AppSearch.vue
+++ app/Components/AppSearch.vue
@@ -5,21 +5,21 @@ const UI = new GamePickerUI();
 </script>
 
 <template>
-	<form @submit.prevent>
-		<input
-			v-model="input"
-			type="text"
-			placeholder="Enter your Steam ID or profile URL"
-			autocomplete="off"
-			spellcheck="false"
-		/>
-		<button
-			type="submit"
-			@click="UI.searchUser(input)"
-			>
-			Search
-		</button>
-	</form>
+	<div class="user">
+		<img :src="UI.user.avatar" alt="User Avatar" />
+		<div class="info">
+			<h2>{{ UI.user.name }}</h2>
+			<p v-if="UI.user.id">Steam ID: {{ UI.user.id }}</p>
+		</div>
+	</div>
+	<input
+		v-model="query"
+		@input="UI.searchUser(query)"
+		type="text"
+		placeholder="Enter your Steam ID or profile URL"
+		autocomplete="off"
+		spellcheck="false"
+	/>
 </template>
 
 <style scoped>
--- app/Controllers/GamePickerUI.ts
+++ app/Controllers/GamePickerUI.ts
@@ -1,34 +1,60 @@
 export class GamePickerUI {
 
+    private timeoutId       : number | undefined;
+    private timeoutDuration : number = 1000;
+
+    public user = reactive ({
+        id          : '',
+        name        : '',
+        avatar      : '',
+        });
+
     public searchUser(query: string) : void {
-        if (query.includes("steamcommunity.com")) {
-            this.searchUserByProfileUrl(query);
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
             }
-        else {
-            this.searchUserById(query)
-            }
+
+        this.timeoutId = window.setTimeout(() => {
+            if (query.includes("steamcommunity.com")) {
+                this.searchUserByProfileUrl(query);
+                }
+            else {
+                this.searchUserById(query)
+                }
+            }, this.timeoutDuration);
         }
 
     private searchUserByProfileUrl(sUrl: string) : void {
         const url = new URL(sUrl);
         const vanity = url.pathname.split('/')[2];
-        fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${this.STEAM_API_KEY}&steamids=${vanity}`, {
+        fetch(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${this.STEAM_API_KEY}&vanityurl=${vanity}`, {
             method: 'GET',
             })
-            .then(response =>
-                response.json()
+            .then(res =>
+                res.json()
                 )
             .then(data => {
-                if (data.response.steamid) {
-                    this.STEAM_ID_64 = data.response.steamid;
-                    this.searchUserById(this.STEAM_ID_64);
-                    }
-                else {
-                    console.error("User not found:", data);
-                    }
+                fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${this.STEAM_API_KEY}&steamids=${data.response.steamid}`, {
+                    method: 'GET',
+                    })
+                    .then(response =>
+                        response.json()
+                        )
+                    .then(data => {
+                        const player = data.response.players[0];
+
+                        if (player) {
+                            this.user.name   = player.personaname;
+                            this.user.id     = player.steamid;
+                            this.user.avatar = player.avatarfull; // or avatarmedium / avatar depending on size you want
+                            }
+                        })
+                    .catch(err =>
+                        console.error("Error fetching player summaries!", err)
+                        );
                 })
-            .catch(error =>
-                console.error('Error fetching player summaries:', error)
+            .catch(err =>
+                console.error("Can't find a user with specified Steam ID!:", err)
                 );
         }
 
@@ -40,11 +66,29 @@ export class GamePickerUI {
                 res.json()
                 )
             .then(data => {
-                const player = data.response.players[0];
-                console.log("User found:", player);
+                fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${this.STEAM_API_KEY}&steamids=${data.response.steamid}`, {
+                    method: 'GET',
+                    })
+                .then(response =>
+                    response.json()
+                    )
+                .then(data => {
+                    const player = data.response.players[0];
+
+                    if (player) {
+                        this.user.name   = player.personaname;
+                        this.user.id     = player.steamid;
+                        this.user.avatar = player.avatarfull;
+
+
+                        console.log(this.user);}
+                    })
+                .catch(err =>
+                    console.error("Error fetching player summaries!", err)
+                    );
                 })
             .catch(err =>
-                console.error("Error fetching player summaries:", err)
+                console.error("Can't find a user with specified Steam ID!:", err)
                 );
         }
     }
